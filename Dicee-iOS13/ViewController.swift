af513b5cc566bd75792b5cbd6ed5dee13dc475c9
JTSwisher <jefftswisher@gmail.com>
2021-03-03 09:53:54 -0700

    finished proj

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    //InterfaceBuilderOutlet
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//       Do any additional setup after loading the view.
+//       who            what   value
+//       diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
+//       diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
+//    }
+    
+    //InterfaceBuilderAction
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        diceImageViewOne.image = diceArray[Int.random(in: 0...5)]
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
+//        Alternate
+//        diceImageViewOne.image = diceArray.randomElement()
+//        diceImageViewOne.image = diceArray.randomElement()
     }
-
-
+    
 }
-
